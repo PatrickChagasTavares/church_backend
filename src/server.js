20574868a6b63fa8154b5509396d50cf72b2daf6
Patrick Chagas <chagas@tradersclub.com.br>
2020-02-20 21:29:26 -0300

    adicionar padroes para o cod

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
-import app from "./app";
+import app from './app';
 
-app.listen(3333, function() {
-  console.log("Viagem Missionaria em : 3333");
+app.listen(3333, () => {
+  console.log('Viagem Missionaria em : 3333');
 });
